167c8722414beaec2c5cb638f7350cb1f6e1bbd7
bala-amypo <bala@amypo.in>
2025-12-17 05:22:24 +0000

    User commit

--- src/main/java/com/example/demo/Controller/StudentController.java
+++ src/main/java/com/example/demo/Controller/StudentController.java
@@ -4,5 +4,22 @@ public class Studct1{
     @Autowired
     private Studservice ser;
     @PostMapping("/add")
-    
+    public Studentity addStudent(@RequestBody Studentity st){
+        return ser.saveData(st);
+
+    }
+    @GetMapping("/getall")
+    public Collection<Studentity> getAllStudents(){
+        return ser.getAll();
+
+    }
+    @getMapping("/get/{id}");
+    public Studentity getStudentById(@PathVariable int id){
+        return ser.getById(id);
+
+    }
+    @PutMapping("/update/{id}")
+    public Studentity updateStudent{
+        
+    }
 }
