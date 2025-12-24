acd38a31d0f9d790ac4a68025cf80e1649230b89
bala-amypo <bala@amypo.in>
2025-12-24 08:18:25 +0000

    User commit

--- src/main/java/com/example/demo/Service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/Service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.Service.impl;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.Service.UserService;
 import com.example.demo.Repository.UserRepo;
 import com.example.demo.Entity.UserEntity;
@@ -9,19 +11,28 @@ import com.example.demo.Entity.UserEntity;
 public class UserServiceImpl implements UserService {
 
     private final UserRepo repo;
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public UserServiceImpl(UserRepo repo) {
         this.repo = repo;
     }
 
+    @Override
     public UserEntity register(UserEntity user) {
+
         if (repo.findByEmail(user.getEmail()).isPresent()) {
+            // 🔴 MUST MATCH QUESTION EXACTLY
             throw new RuntimeException("Email already exists");
         }
+
+        user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
     }
 
+    @Override
     public UserEntity findByEmail(String email) {
-        return repo.findByEmail(email).orElse(null);
+        return repo.findByEmail(email)
+                .orElseThrow(() ->
+                        new RuntimeException("UserProfile not found"));
     }
 }
