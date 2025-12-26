decbc71be818218c0a77e11ad97fed58da69c70a
bala-amypo <bala@amypo.in>
2025-12-26 16:02:42 +0000

    User commit

---
+++ src/main/java/com/example/barter/service/BarterTransactionService.java
@@ -0,0 +1,8 @@
+package com.example.barter.service;
+
+import com.example.barter.model.BarterTransaction;
+
+public interface BarterTransactionService {
+
+    BarterTransaction create(BarterTransaction transaction);
+}
