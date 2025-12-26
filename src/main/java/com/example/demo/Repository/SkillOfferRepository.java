b149578cfb217090e4763a2cc828baff2180993e
bala-amypo <bala@amypo.in>
2025-12-26 09:52:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/Repository/SkillOfferRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.SkillOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface SkillOfferRepository extends JpaRepository<SkillOffer, Long> {
+
+    List<SkillOffer> findByUserId(Long userId);
+}
---
+++ src/main/java/com/example/demo/Repository/SkillRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+}
